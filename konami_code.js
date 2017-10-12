b6bbb11e7c49402aa75372225358d356712442fa
sukey <sdklos@gmail.com>
2017-10-11 21:16:28 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,21 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index = 0;
+  const body = document.body
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    console.log(e.which)
+    if (key === code[index]) {
+      index++;
 
+      if(index === code.length) {
+        alert("This is not a test!!! Get out while you still can!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  })
 }
